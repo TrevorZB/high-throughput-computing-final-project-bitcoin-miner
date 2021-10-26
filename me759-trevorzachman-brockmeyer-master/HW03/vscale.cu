e671f592dbec0289636de0d13f2d81b60c56f8ce
TrevorZB <trevorzb11@gmail.com>
2021-10-25 20:04:30 -0500

    removed duplicate homework folders from final project repo

--- me759-trevorzachman-brockmeyer-master/HW03/vscale.cu
+++
@@ -1,8 +0,0 @@
-__global__ void vscale(const float *a, float *b, unsigned int n)
-{
-    int index = blockDim.x * blockIdx.x + threadIdx.x;
-    if (index < n)
-    {
-        b[index] *= a[index];
-    }
-}
